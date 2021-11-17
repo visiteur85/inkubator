822e04ea024c5525659c4b4a8f4427f86815b537
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-17 16:55:27 +0300

    Social_L08

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-<ul>
-  <li>css</li>
-  <li>html</li>
-  <li>JS</li>
-  <li>react</li>
-</ul>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
+const Technologies = () => {
+    return (
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>JS</li>
+            <li>react</li>
+        </ul>
+    )
+}
+
+const Header = () => {
+    return <div>
+        <a href="#">Home</a>
+        <a href="#">News Feed</a>
+        <a href="#">Messages</a>
+    </div>
+}
 export default App;
