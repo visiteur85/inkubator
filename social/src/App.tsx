073b0716158214c27dc45bb544863b288eb83a7f
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-17 15:44:33 +0300

    Social-les06

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<ul>
+  <li>css</li>
+  <li>html</li>
+  <li>JS</li>
+  <li>react</li>
+</ul>
     </div>
   );
 }
