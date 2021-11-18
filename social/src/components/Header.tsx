54bbf4a06188ac618a2e6bff20762a28436cb10e
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-18 14:31:46 +0300

    Social_les14

--- social/src/components/Header.tsx
+++ social/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import s from "./Header.module.css"
 export const Header = () => {
-    return <header className="header">
+    return <header className={s.header}>
         <img
             src="https://upload.wikimedia.org/wikipedia/ru/c/cf/%D0%9B%D0%BE%D0%B1%D0%BE%D1%81_%D0%A3%D0%9F%D0%9D%D0%A4%D0%9C_%28%D0%BB%D0%BE%D0%B3%D0%BE%29.png"/>
     </header>
