5bd462577c9aebc6483528ae9123afe71026db66
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-20 01:03:21 +0300

    social_les-20-navlink

--- social/src/components/Navbar/Navbar.tsx
+++ social/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from "./Navbar.module.css"
 export const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}><a href="/profile">Profile</a></div>
-        <div className={s.item}><a href="/dialogs">Messages</a></div>
-        <div className={s.item}><a href="/news">News</a></div>
-        <div className={s.item}><a href="/music">Music</a></div>
-        <div className={s.item}><a href="/settings">Settings</a></div>
+        <div className={s.item}><NavLink  to="/profile" className={({isActive}) => isActive ? s.active : s.item}>Profile</NavLink></div>
+        <div className={s.item}><NavLink to="/dialogs" className={({isActive}) => isActive ? s.active : s.item}>Messages</NavLink></div>
+        <div className={s.item}><NavLink to="/news">News</NavLink></div>
+        <div className={s.item}><NavLink to="/music">Music</NavLink></div>
+        <div className={s.item}><NavLink to="/settings">Settings</NavLink></div>
     </nav>
 }
+
+// activeClassName={"s.activeLink"}
