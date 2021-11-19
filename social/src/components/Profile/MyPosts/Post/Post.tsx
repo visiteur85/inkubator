c5fe8aeedad2e20e176fa4ab02ba3f5135dd9b26
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-19 13:43:42 +0300

    Social_Les18

--- social/src/components/Profile/MyPosts/Post/Post.tsx
+++ social/src/components/Profile/MyPosts/Post/Post.tsx
@@ -13,7 +13,7 @@ export const Post: React.FC <PostTypeProps> = (props) => {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq7ktobmis68f6L18AowLqKzIAFN-16Je2q5v2fyaeW7e3M9EHZkPDxg9qPIYjpSWW4wE&usqp=CAU"/>
         {props.message}
         <div>
-            <span>{props.likes}</span>
+            <span> like {props.likes}</span>
         </div>
 
     </div>
--- social/src/components/Profile/Profile.tsx
+++ social/src/components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import  s from "./Profile.module.css"
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src="https://media.istockphoto.com/photos/eiffel-tower-in-paris-skyline-at-dawn-picture-id1280246120?b=1&k=20&m=1280246120&s=170667a&w=0&h=9g8hb-FTp7TfrN6gItpxDFKG0wPjwvnZQlNHZaxyeeI="/>
         </div>
