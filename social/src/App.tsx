defe289005300e5cfb4aff7606ec0a20be862b3d
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-19 22:00:29 +0300

    social_les19_route

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -4,19 +4,28 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            {/*<Profile/>*/}
-            <div className="app-wrapper-content">
-                <Dialogs/>
-                {/*<Profile/>*/}
+
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                        {/*<Route path="/news" element={<News/>}/>*/}
+                        {/*<Route path="/music" element={<Music/>}/>*/}
+                        {/*<Route path="/settings" element={<Settings/>}/>*/}
+                    </Routes>
+                </div>
             </div>
-        </div>
+
     );
 }
 
