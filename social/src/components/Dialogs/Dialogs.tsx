fb55b86c94fe57bb411890aa0256cdeceef118a2
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-20 13:03:00 +0300

    Social_les21

--- social/src/components/Dialogs/Dialogs.tsx
+++ social/src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,20 @@ import  s from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + " " + s.active}>Pavel</div>
+                <div className={s.dialog}>Andrew</div>
+                <div className={s.dialog}>Sasha</div>
+                <div className={s.dialog}>Viktor</div>
+                <div className={s.dialog}>Denis</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
+
         </div>
     )
 
