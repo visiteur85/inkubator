0a930695da595141ee3984f20bbc4c1efc01294b
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-11-22 19:54:50 +0300

    Social_22

--- social/src/components/Dialogs/Dialogs.tsx
+++ social/src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import  s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <div className={s.dialog + " " + s.active}>Pavel</div>
-                <div className={s.dialog}>Andrew</div>
-                <div className={s.dialog}>Sasha</div>
-                <div className={s.dialog}>Viktor</div>
-                <div className={s.dialog}>Denis</div>
+                <div className={s.dialog + " " + s.active}>
+                    <NavLink to="/dialogs/1">Pavel</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Andrew</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Sasha</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Viktor</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Denis</NavLink></div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
