201bf040c4ea745b4aaf4c154e77beeb63533904
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-12-06 14:05:03 +0300

    Social_24

--- social/src/components/Dialogs/Dialogs.tsx
+++ social/src/components/Dialogs/Dialogs.tsx
@@ -29,24 +29,42 @@ const Message = (props: MessagePropsType) => {
 
 
 export const Dialogs = () => {
+
+    let dialogData = [
+        {id: 1, name: "Pavel"},
+        {id: 2, name: "Alex"},
+        {id: 3, name: "Ivan"},
+        {id: 4, name: "Olia"},
+        {id: 5, name: "Serge"},
+        {id: 6, name: "Tania"},
+    ]
+
+    let messagesData = [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How is your It -kamasutra"},
+        {id: 3, message: "Yo"},
+        {id: 4, message: "Yo"},
+        {id: 5, message: "Yo"},
+        {id: 6, message: "Yo"},
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
 
-                <DialogItem name={"Pavel"} id={1}/>
-                <DialogItem name={"Alex"} id={2}/>
-                <DialogItem name={"Ivan"} id={3}/>
-                <DialogItem name={"Olia"} id={4}/>
-                <DialogItem name={"Serge"} id={5}/>
-                <DialogItem name={"Tania"} id={6}/>
+                <DialogItem name={dialogData[0].name} id={0}/>
+                <DialogItem name={dialogData[1].name} id={1}/>
+                {/*<DialogItem name={"Ivan"} id={3}/>*/}
+                {/*<DialogItem name={"Olia"} id={4}/>*/}
+                {/*<DialogItem name={"Serge"} id={5}/>*/}
+                {/*<DialogItem name={"Tania"} id={6}/>*/}
 
             </div>
             <div className={s.messages}>
-                <Message message={"Hi"}/>
-                <Message message={"How are you"}/>
-                <Message message={"Yo"}/>
-                <Message message={"Yo"}/>
-                <Message message={"Yo"}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                {/*<Message message={"Yo"}/>*/}
+                {/*<Message message={"Yo"}/>*/}
+                {/*<Message message={"Yo"}/>*/}
 
             </div>
 
--- social/src/components/Profile/MyPosts/MyPosts.tsx
+++ social/src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,13 @@ import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: "Hi, how are you", likesCount: 12 },
+        {id: 2, message: "It is  my first post", likesCount: 11 },
+
+    ]
+
     return <div className={s.postsBlock}>
         <h3>My posts</h3>
         <div>
@@ -14,8 +21,8 @@ export const MyPosts = () => {
             </div>
         </div>
         <div className={s.posts}>
-            <Post message="Hi, how are you" likes={15}/>
-            <Post message="It is  my first post" likes={20}/>
+            <Post message={postData[0].message} likes={postData[0].likesCount}/>
+            <Post message={postData[1].message} likes={postData[1].likesCount}/>
 
 
         </div>
