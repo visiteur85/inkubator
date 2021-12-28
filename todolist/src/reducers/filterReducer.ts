39d0f0695eda882fc4abb2ecbe3e1e208398dd59
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2021-12-28 21:17:07 +0300

    Todolist 7_useReducer

---
+++ todolist/src/reducers/filterReducer.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FilterValuesType} from "../App";
+
+export const FilterReducer = (state: FilterValuesType, action: changeFilterACType) => {
+    switch (
+        action.type){
+        case "CHANGE-FILTER": {
+            return action.payload.value
+        }
+        default: return state
+    }
+};
+type changeFilterACType = ReturnType<typeof  changeFilterAC>
+export const changeFilterAC = (value: FilterValuesType) => {
+return {
+    type: "CHANGE-FILTER",
+    payload: {
+        value: value
+    }
+} as const
+}
+
