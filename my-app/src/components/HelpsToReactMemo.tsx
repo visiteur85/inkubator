6ea2575fa9ba203c64d45dfe6df7dae96e70cad7
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-02-15 19:23:49 +0300

    feat: kabzda_UseCallback

--- my-app/src/components/HelpsToReactMemo.tsx
+++ my-app/src/components/HelpsToReactMemo.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 
 
 export const UsersSecret = (props: { users: Array<string> }) => {
@@ -23,6 +23,50 @@ export const HelpsToReactMemo = () => {
     );
 };
 
+
+
+
+export const LikeUseCallback = () => {
+    const [counter, setCounter] = useState(0);
+    const [book, setBooks] = useState(["React", "CSS", "JS"]);
+
+    // const newArray =  useMemo(()=>{return book.filter(u=>u.toLowerCase().indexOf("a")>-1)},[])
+
+   const addBook = ()=> {
+        const newBook = [...book, "ANGULAR" + new Date().getTime()];
+        setBooks(newBook)
+    }
+
+ const memorized = useMemo(()=> {
+     return ()=> {
+         const newBook = [...book, "ANGULAR" + new Date().getTime()];
+         setBooks(newBook)
+     }
+ },[book]);
+
+    const memorized2 = useCallback(()=> {
+                    const newBook = [...book, "ANGULAR" + new Date().getTime()];
+            setBooks(newBook)
+
+    },[book]);
+
+
+    return (
+        <div>
+            <button onClick={()=>setCounter(counter+1)}>+</button>
+            {counter}
+            <Book  addBook={addBook} />
+        </div>
+    );
+};
+
+export const BooksSecret = (props: { addBook:()=>void }) => {
+    return <div>
+        <button onClick={()=>props.addBook}>+</button>
+    </div>
+
+}
+const Book = React.memo(BooksSecret);
 
 
 
