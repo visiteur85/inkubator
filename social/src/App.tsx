edc3d3409911db783b808b9ee0a5b3dd21cc37ad
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-03-14 20:23:24 +0300

    add ver2 - les 11

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -1,53 +1,40 @@
 import React from 'react';
-import './App.css';
-import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
-
-import {Route, Routes} from "react-router-dom";
-import {BrowserRouter} from "react-router-dom";
-import {AppStoreType, store,} from "./redux/redux-store";
-import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
-import {Newss} from "./components/News/Newss";
+import "./App.css"
 
 
-
-// type PropsType = {
-//     store: AppStoreType //взяли из редакс строка 13
-// }
 const App = () => {
-    const state = store.getState()
+
 
     return (
-        // <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path="/dialogs" element={<DialogsContainer
-
-                            // dialogs={state.dialogPage.dialogs}
-                            // messages={state.dialogPage.messages}
-                            // newMessageBody={state.dialogPage.newMessageBody}
-                            // dispatch={props.store.dispatch.bind(props.store)}
-
-                         />}/>
-                        <Route path="/profile" element={<Profile
-                            // post={state.profilePage.post}
-                            // dispatch={props.store.dispatch.bind(props.store)}
-                            // store={props.store}
-                            // message={state.profilePage.messageForNewPost
-                            // }
-
-                        />}/>
-                        {/*<Route path="/news" element={<News />}/>*/}
-
-                    </Routes>
-
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"
+                    alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a href="">Profile</a></div>
+                <div><a href="">Messages</a></div>
+                <div><a href="">News</a></div>
+                <div><a href="">Music</a></div>
+                <div><a href="">Settings</a></div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img
+                        src="https://ichef.bbci.co.uk/news/640/cpsprodpb/FE55/production/_105090156_gettyimages-836351108.jpg"
+                        alt="space"/>
+                </div>
+                <div>ava+description</div>
+                <div>My Posts
+                    <div>New Post</div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                    </div>
                 </div>
             </div>
-        // </BrowserRouter>
+        </div>
     );
 }
 
