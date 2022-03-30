62142cb92c8a454130d53c49c80f5d3d5cb31958
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-03-30 18:57:28 +0300

    add ver2 - les 27

--- social/src/App.tsx
+++ social/src/App.tsx
@@ -9,6 +9,7 @@ import {Routes, Route} from "react-router-dom";
 
 const App = () => {
 
+    let text = "aaa";
 
     return (
 
@@ -19,6 +20,8 @@ const App = () => {
                 <Routes>
                     <Route path={"/dialogs"} element={<Dialogs/>}/>
                     <Route path={"/profile"} element={<Profile/>}/>
+
+
                     {/*<Route path={"/news"} element={<News/>}/>*/}
                     {/*<Route path={"/music"} element={<Music/>}/>*/}
                     {/*<Route path={"/settings"} element={<Settings/>}/>*/}
