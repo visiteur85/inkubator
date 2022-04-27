3c58b6a5ebeaca2d03ffefa55df5f69504c80a22
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-04-27 19:44:27 +0300

    add ver2 - les 54

--- social/src/componets/Users/Users.tsx
+++ social/src/componets/Users/Users.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import {OneUserType} from "../../Redux/users-reducer";
 import s from "./users.module.css"
 import axios from "axios";
-import {MapStateToPropsType} from "../Dialogs/DialogsContainer";
-import {MapDispatchToPropsType} from "./UsersContainer";
-import {RootReducerType} from "../../Redux/redux-store";
-import {Dispatch} from "redux";
+
 
 type PropsType = {
     items: Array<OneUserType>;
@@ -15,33 +12,27 @@ type PropsType = {
 };
 
 
-
 export class Users extends React.Component<PropsType> {
 
-    constructor(props:PropsType) {
-
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users", {
-                headers: {
-                    'API-KEY': '13291219-4788-4555-a4f4-aaeffe0abc09'
-                }
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            headers: {
+                'API-KEY': '13291219-4788-4555-a4f4-aaeffe0abc09'
+            }
+        })
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
 
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                })
+    }
 
-        }
+    render() {
+        return (
+            <div>
+                {/*<button onClick={this.getUsers}> get Users</button>*/}
+                {this.props.items.map((user: OneUserType) => {
 
-
-
-render() {
-    return (
-        <div>
-            {/*<button onClick={this.getUsers}> get Users</button>*/}
-            {this.props.items.map((user:OneUserType) => {
-
-                return <div key={user.id}>
+                    return <div key={user.id}>
           <span>
             <div>
               <img src={user.photos.small !== null ? user.photos.small :
@@ -60,7 +51,7 @@ render() {
 
             </div>
           </span>
-                    <span>
+                        <span>
             <span>
               <div>{user.name}</div>
               <div>{user.status}</div>
@@ -74,9 +65,9 @@ render() {
               </div>
             </span>
           </span>
-                </div>
-            })}
-        </div>
-    );
-}
+                    </div>
+                })}
+            </div>
+        );
+    }
 }
--- social/src/componets/Users/UsersContainer.tsx
+++ social/src/componets/Users/UsersContainer.tsx
@@ -1,39 +1,39 @@
-import { connect } from "react-redux";
-import React  from "react";
-import { Users } from "./Users";
-import { RootReducerType } from "../../Redux/redux-store";
-import { followAC, OneUserType, setUserAC, unfollowAC } from "../../Redux/users-reducer";
-import { Dispatch } from "redux";
+import {connect} from "react-redux";
+import React from "react";
+import {Users} from "./Users";
+import {RootReducerType} from "../../Redux/redux-store";
+import {followAC, OneUserType, setUserAC, unfollowAC} from "../../Redux/users-reducer";
+import {Dispatch} from "redux";
 
 type MapStateToPropsType = {
-  items: Array<OneUserType>;
+    items: Array<OneUserType>;
 };
 const mapStateToProps = (state: RootReducerType): MapStateToPropsType => {
-  return {
-    items: state.usersPage.items
-  };
+    return {
+        items: state.usersPage.items
+    };
 };
 
 export type MapDispatchToPropsType = {
-  follow:(userId: number)=>void
-  unFollow:(userId: number)=>void
-  setUsers: (items: Array<OneUserType>)=>void
+    follow: (userId: number) => void
+    unFollow: (userId: number) => void
+    setUsers: (items: Array<OneUserType>) => void
 };
-const mapDispatchToProps = (dispatch:Dispatch): MapDispatchToPropsType => {
-  return {
-    follow: (userId: number) => {
-      dispatch(followAC(userId));
-    },
-    unFollow: (userId: number) => {
-      dispatch(unfollowAC(userId));
-    },
-    setUsers: (users: Array<OneUserType>)=> {
-      dispatch(setUserAC(users))
-    }
-  };
+const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
+    return {
+        follow: (userId: number) => {
+            dispatch(followAC(userId));
+        },
+        unFollow: (userId: number) => {
+            dispatch(unfollowAC(userId));
+        },
+        setUsers: (users: Array<OneUserType>) => {
+            dispatch(setUserAC(users))
+        }
+    };
 };
 
 export const UsersContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Users);
