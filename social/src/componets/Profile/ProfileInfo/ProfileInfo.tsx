fd9ba7d25726693d1aa9463cf34fdc2ab97d8c22
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-06-05 18:04:11 +0300

    add ver2 - les 71

--- social/src/componets/Profile/ProfileInfo/ProfileInfo.tsx
+++ social/src/componets/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from "./profileinfo.module.css"
 import {Preloader} from "../../common/preloader/Preloader";
 import {ProfileFromServerType} from "../../../Redux/profile-reducer";
+import {ProfileStatus} from "./ProfileStatus"
 
 
 type PropsType = {
@@ -15,14 +16,15 @@ export const ProfileInfo = (props:PropsType) => {
 
     return (
         <div >
-            <div>
-                <img
-                    src="https://ichef.bbci.co.uk/news/640/cpsprodpb/FE55/production/_105090156_gettyimages-836351108.jpg"
-                    alt="space"/>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src="https://ichef.bbci.co.uk/news/640/cpsprodpb/FE55/production/_105090156_gettyimages-836351108.jpg"*/}
+            {/*        alt="space"/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.small}/>
-                ava+description</div>
+              <ProfileStatus status={"Hello my friends"}/>
+            </div>
         </div>
 
 
