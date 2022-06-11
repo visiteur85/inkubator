b509aadd4c24376a8b91b1541d2cbffe4fd74f91
admin <krivalcevich@gmail.com>
2022-06-11 15:00:23 +0300

    Social 75

--- social/src/Redux/redux-store.ts
+++ social/src/Redux/redux-store.ts
@@ -1,21 +1,25 @@
 import {usersReducer} from './users-reducer';
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers,createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialog-reducer";
 import {sideBarReducer} from "./sideBar-reducer";
 import {authReducer} from "./auth-reducer";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from "redux-form"
 
 export let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sideBarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 
 })
 export type RootReducerType = ReturnType<typeof rootReducer>
 export type StoreType = typeof store
 
-export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware) );
+
+
 
--- social/src/componets/Login/Login.tsx
+++ social/src/componets/Login/Login.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
-export const Login = () => {
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+
+}
+
+export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
-       <h1>LOGIN</h1>
+
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
 
 
     );
 };
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    form: "login"
+})(LoginForm)
+
+export const Login = () => {
+
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+    return (
+        <div>
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+
+
+    );
+};
+
+
 
