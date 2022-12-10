ae0f3d0c2539f765b7522a32bd5f3852971f4d90
admin <krivalcevich@gmail.com>
2022-12-10 11:01:08 +0300

    1234

--- social/src/componets/Profile/Profile.tsx
+++ social/src/componets/Profile/Profile.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import s from "./profile.module.css";
-import {MyPosts} from "./My posts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {} from "../../App";
-// import {ActionsType, ProfilePageType} from "../../Redux/store";
 
 
 import {MyPostsContainer} from "./My posts/MyPostsContainer";
